edd6ec056a13263c85f6fafd98958a3b5c93ea1e
fenger <fenger@fenger.yu>
2015-01-04 21:19:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+mvn archetype:generate
+  -DgroupId=[your project's group id]
+  -DartifactId=[your project's artifact id]
+  -DarchetypeArtifactId=maven-archetype-archetype
+  
+  
+
+ 可以用map.get判断是否存在key吗？
+ 不可以，因为map.get值不存在是为空，当key对应的value是null时也为空
+ 
+ 
---
+++ side-guava/src/main/java/org/fenger/App.java
@@ -0,0 +1,16 @@
+package org.fenger;
+
+import com.google.common.base.Objects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Objects.hashCode(new String("fenger"));
+    }
+}
